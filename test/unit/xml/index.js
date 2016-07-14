2e47e3e27f772ea3537c3ccfe23592da5336ad76
Jerry Hamlet <jerry@hamletink.com>
2016-07-14 23:26:44 +0200

    Separate sanitize from the actual xml parsing

--- test/unit/xml/index.js
+++ test/unit/xml/index.js
@@ -3,13 +3,41 @@ require('should');
 
 describe('svg-react-loader/lib/xml', () => {
     const xml = require('../../../lib/xml')();
+    const sanitize = require('../../../lib/xml/sanitize')(null);
     const read = require('../../../lib/util/read-file');
 
     it('should parse xml correctly', (done) => {
         read('test/unit/xml/simple.svg').
             flatMap(xml.parse).
             subscribe(
-                (result) => { console.log(JSON.stringify(result, null, 4)); },
+                (result) => {
+                    sanitize(result).
+                        should.
+                        eql({
+                            "props": {
+                                "version": "1.1",
+                                "x": "0px",
+                                "y": "0px",
+                                "viewbox": "0 0 16 16",
+                                "enableBackground": "new 0 0 16 16",
+                                "xmlSpace": "preserve",
+                                "className": "simple"
+                            },
+                            "tagname": "svg",
+                            "children": [
+                                {
+                                    "props": {
+                                        "x": "0",
+                                        "y": "0",
+                                        "width": "16",
+                                        "height": "16",
+                                        "fill": "#fff"
+                                    },
+                                    "tagname": "rect"
+                                }
+                            ]
+                        });
+                },
                 (error) => { throw error; },
                 done
             );
